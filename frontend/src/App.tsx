43050af4ae72f4e9916078e06d17bda10fb4efb5
Ítalo Gregório <italo-07@hotmail.com>
2022-11-15 17:49:13 -0300

    Created Sales Card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
 
   return (
     <>
       <Header />
-      <h1>Olá mundo</h1>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
