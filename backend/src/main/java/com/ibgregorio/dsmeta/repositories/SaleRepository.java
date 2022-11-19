83a34cac0bfa11c23486e4b7e9a9c712b9a275b1
Ítalo Gregório <italo-07@hotmail.com>
2022-11-18 21:43:02 -0300

    Created Sale repository

---
+++ backend/src/main/java/com/ibgregorio/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.ibgregorio.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ibgregorio.dsmeta.entities.Sale;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
