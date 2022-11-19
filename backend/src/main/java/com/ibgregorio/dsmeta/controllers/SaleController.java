87db6c82fd9e2a66cdc3944c8936be077d90dca5
Ítalo Gregório <italo-07@hotmail.com>
2022-11-18 21:43:19 -0300

    Created Find Sales endpoint

---
+++ backend/src/main/java/com/ibgregorio/dsmeta/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.ibgregorio.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibgregorio.dsmeta.entities.Sale;
+import com.ibgregorio.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+}
---
+++ backend/src/main/java/com/ibgregorio/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.ibgregorio.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibgregorio.dsmeta.entities.Sale;
+import com.ibgregorio.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+}
