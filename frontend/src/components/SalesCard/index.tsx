68c960d63595df94855bb9f0cb2c10c7f99bd220
Ítalo Gregório <italo-07@hotmail.com>
2022-11-20 16:04:53 -0300

    Added Axios and implemented API callback on useEffect hook

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import NotificationButton from '../NotificationButton'
 
 import "react-datepicker/dist/react-datepicker.css";
 import './styles.css'
+import axios from "axios";
 
 function SalesCard() {
 
@@ -12,6 +13,14 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+            .then(response => {
+                console.log(response.data);
+                
+            })
+    }, [])
 
     return (
         <div className="dsmeta-card">
